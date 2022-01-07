9a4bf471621bcd15616b8fdf2ca8049c5e1fb609
chrismelyan <chris.shelpuk@gmail.com>
2022-01-08 00:10:00 +0100

    final changes
    corrected mistakes

--- script.js
+++ script.js
@@ -74,28 +74,19 @@ function detectPersonalLevel() {
 
 detectPersonalLevel();
 
-function showMyDB(privat) {
-     return privat == false ? personalMoviesDB : "none";
+function showMyDB(hidden) {
+     if (!hidden) {
+      console.log(personalMoviesDB);
+     } 
 }
 
-showMyDB();
+showMyDB(personalMoviesDB.privat);
 
 function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        const a = prompt('Ваш любимый жанр под номером ${}', '');
-        a = personalMoviesDB.genres[];
+    for (let i = 1; i < 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMoviesDB.genres[i - 1] = genre;
     }
 }
 
-function writeYourGenres();
-/* Задание на урок:
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
+writeYourGenres();
