f760318468383951ba6f60fe3d96aa9c512c4f85
chrismelyan <chris.shelpuk@gmail.com>
2022-01-09 22:45:57 +0100

    add counter

--- study.js
+++ study.js
@@ -13,14 +13,16 @@ const options = {
 //delete options.name;
 
 //console.log(options);
-
+let counter = 0;
 for (let key in options) {
         if (typeof(options[key]) === 'object') {
             for (let i in options[key]) {
             console.log(`Свойство ${i} имеет знаение ${options[key][i]}`);  
-            }
+            } counter++;
         } else {
         console.log(`Свойство ${key} имеет знаение ${options[key]}`);
-    }
+    } counter++;
 }
+
+console.log(counter);
     
