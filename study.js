5c1e5a86c44ea8cba513f68bb9d2c9ccba95db4a
chrismelyan <chris.shelpuk@gmail.com>
2022-01-09 22:33:26 +0100

    search loop

--- study.js
+++ study.js
@@ -8,4 +8,19 @@ const options = {
     }
 };
 
-console.log(options.name);
+//console.log(options.name);
+
+//delete options.name;
+
+//console.log(options);
+
+for (let key in options) {
+        if (typeof(options[key]) === 'object') {
+            for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет знаение ${options[key][i]}`);  
+            }
+        } else {
+        console.log(`Свойство ${key} имеет знаение ${options[key]}`);
+    }
+}
+    
