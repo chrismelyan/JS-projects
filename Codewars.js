e57a2345845c42883bd8a63e0b4d2f1517efb23d
chrismelyan <chris.shelpuk@gmail.com>
2022-03-02 22:40:05 +0100

    Sort the odd (6)

--- Codewars.js
+++ Codewars.js
@@ -245,6 +245,24 @@ function countSmileys(arr) {
     if (RegexObj.test(face)) {
       inc++
     }
-    return inc;
   }
+  return inc;
+}
+//arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+
+//Sort the odd (6)
+function sortArray(array) {
+  let oddArr = [];
+  let evenArr = [];
+  let resultArr = [];
+  for (let i = 0; i < array.length; i++) {
+    array[i]%2 === 0 ? evenArr.push(array[i]) : oddArr.push(array[i])
+  }
+  
+  oddArr.sort((a, b) => a-b)
+  
+  for (let i = 0; i < array.length; i++) {
+    array[i]%2 === 0 ? resultArr.push(evenArr.shift()) : resultArr.push(oddArr.shift())
+  }
+  return resultArr
 }
